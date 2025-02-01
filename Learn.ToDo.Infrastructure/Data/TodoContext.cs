2ddd61159f6794c256104a8b0f50a9064f821a2e
Hamza Mekouar <visual.workcode@outlook.com>
2025-02-01 23:21:49 +0100

    Done

--- Learn.ToDo.Infrastructure/Data/TodoContext.cs
+++ Learn.ToDo.Infrastructure/Data/TodoContext.cs
@@ -11,7 +11,7 @@ namespace Learn.ToDo.Infrastructure.Data
     public class TodoContext : DbContext
     {
         private readonly static string MyConString = "Server=DESKTOP-TKBJLN8\\SQLEXPRESS;Database=LearnTodoApiApp2;Trusted_Connection=True;TrustServerCertificate=True;";
-        //"Server=DESKTOP-TKBJLN8\\SQLEXPRESS;Database=LearnTodoApiApp;User=username;Password=123456789Aa;TrustServerCertificate=True;";
+        
         public TodoContext()
         {
             
@@ -22,15 +22,6 @@ namespace Learn.ToDo.Infrastructure.Data
             base.OnConfiguring(optionsBuilder);
             optionsBuilder.UseSqlServer(MyConString);
         }
-
-        /*protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<UserDetail>().HasKey(x=> x.Id);
-            modelBuilder.Entity<ToDoDetail>().HasKey(x=> x.Id);
-            modelBuilder.Entity<ToDoCategory>().HasKey(x=> x.Id);
-            modelBuilder.Entity<Category>().HasKey(x=> x.Id);
-        }*/
 
         public DbSet<UserDetail> UserDetails { get; set; }
         public DbSet<ToDoDetail> ToDoDetails { get; set; }
